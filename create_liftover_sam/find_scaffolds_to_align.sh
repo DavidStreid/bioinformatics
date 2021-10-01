2fa4d5785a12941232e2f5b5db67a33a76a24fc9
DavidStreid <david.streid.nyc@gmail.com>
2021-10-01 11:38:01 -0400

    Finding scaffolds

--- create_liftover_sam/find_scaffolds_to_align.sh
+++ create_liftover_sam/find_scaffolds_to_align.sh
@@ -20,29 +20,48 @@ fi
 LOCATION=$(dirname "$0")
 SCAFFOLD_MAP_DIR="${LOCATION}/alternate_scaffold_mappings"
 ALIGN_DIR=$(pwd)/sams
+SPLIT_DIR=$(pwd)/fasta
+SCFLD_DIR=$(pwd)/scaffolds
+
+
 
 num_contigs=$(cat ${INPUT_REF} | grep ">" | wc -l)
-echo "Writing ${num_contigs} chr*.fa files for ${INPUT_REF}"
 ref_to_scaffold_split_basename="chr"
-faSplit sequence ${INPUT_REF} ${num_contigs} ${ref_to_scaffold_split_basename}
-for f in ${ref_to_scaffold_split_basename}*.fa; do
-  fname=$(head -1 ${f} | cut -d' ' -f1 | sed 's/>//')
-  mv ${f} ${fname}.fa
-done
+
+if [[ -d ${SPLIT_DIR} ]]; then
+  echo "Skipping split. Already wrote ${SPLIT_DIR}"
+else
+  mkdir -p ${SPLIT_DIR}
+  echo "Writing ${num_contigs} chr*.fa files for ${INPUT_REF}"
+  faSplit sequence ${INPUT_REF} ${num_contigs} ${ref_to_scaffold_split_basename}
+  for f in ${ref_to_scaffold_split_basename}*.fa; do
+    fname=$(head -1 ${f} | cut -d' ' -f1 | sed 's/>//')
+    moved_file=${SPLIT_DIR}/${fname}.fa
+    mv ${f} ${moved_file}
+    CMD="/opt/common/CentOS_7/bwa/bwa-0.7.17/bwa index ${moved_file}"
+    echo ${CMD}
+    eval ${CMD}
+  done
+fi
 
 echo "Grabbing all scaffolds from ${INPUT_REF}"
 input_scaffolds=$(cat ${INPUT_REF} | grep ">" | cut -d' ' -f1 | sed 's/>//' | grep -E ".{3,}")
 sMaps=$(find ${SCAFFOLD_MAP_DIR} -type f)
 mapped_scaffolds_suffix="_map_scaffold.txt"
 
+mkdir -p ${SCFLD_DIR}
 for map in ${sMaps}; do
   map_name=$(basename ${map} | cut -d'.' -f1)
   echo "Checking ${map_name}..."
-  scaffolds_to_map_file="${map_name}${mapped_scaffolds_suffix}"
-  touch ${scaffolds_to_map_file}
-  for scaffold in ${input_scaffolds}; do
-    grep "${scaffold}" ${map} >> ${scaffolds_to_map_file}
-  done
+  scaffolds_to_map_file="${SCFLD_DIR}/${map_name}${mapped_scaffolds_suffix}"
+  if [[ -f ${scaffolds_to_map_file} ]]; then
+    echo "Already wrote ${scaffolds_to_map_file}"
+  else
+    touch ${scaffolds_to_map_file}
+    for scaffold in ${input_scaffolds}; do
+      grep "${scaffold}" ${map} >> ${scaffolds_to_map_file}
+    done
+  fi
   num_matches=$(wc -l ${scaffolds_to_map_file} | rev | cut -d' ' -f2 | rev)
   if [[ ${num_matches} -eq 0 ]]; then
     printf "\t... no matches\n"
@@ -55,7 +74,9 @@ for map in ${sMaps}; do
     while IFS= read -r match; do
       qry=$(echo ${match} | cut -d' ' -f2)
       ref=$(echo ${match} | cut -d' ' -f6)
-      CMD="bwa mem -M -t 40 ${ref}.fa ${qry}.fa > ${MAP_ALIGN_DIR}/q${qry}_r${ref}.sam"
+      qry_fa=${SPLIT_DIR}/${qry}.fa
+      ref_fa=${SPLIT_DIR}/${ref}.fa
+      CMD="/opt/common/CentOS_7/bwa/bwa-0.7.17/bwa mem -M -t 40 ${ref_fa} ${qry_fa} > ${MAP_ALIGN_DIR}/q${qry}_r${ref}.sam"
       if [[ ${USE_LOCAL} != "true" ]]; then
         JOB_NAME="ALIGN__Q:${qry}_R:${ref}"
         CMD="bsub -J ${JOB_NAME} -o ${JOB_NAME}.out -n 8 -M 6 '${CMD}'"
