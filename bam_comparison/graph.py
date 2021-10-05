46d903b0e7993aec95329382db1bf47181105271
DavidStreid <david.streid.nyc@gmail.com>
2021-10-05 14:08:06 -0400

    Creating PDFs for any '*_bam_differences.csv'

--- bam_comparison/graph.py
+++ bam_comparison/graph.py
@@ -3,16 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import colors
 from matplotlib.pyplot import figure 
+import glob
 
 figure(figsize=(16,8), dpi=80)
 
-df = pd.read_csv('bam_differences.csv')
-grouped = df.groupby('v1').mean()
-grouped['v1-v2'].plot(kind='bar')
+files = glob.glob('*_bam_differences.csv')
 
-plt.title("BAM Value vs. Average Difference")
-plt.xlabel("BAM1 Score")                    # Or 'BAM2 Score'
-plt.ylabel("Avg(BAM1 Score - BAM2 Score)")  # Avg(BAM2 Score - BAM1 Score) 
-plt.xlim([-2, 62])                          # Helpful for plotting MAPQ, which has range [1-60]
+for f in files:
+    df = pd.read_csv(f)
 
-plt.savefig("bam1_v_diff.pdf")
+    grouped = df.groupby('v1').mean()
+    grouped['v1-v2'].plot(kind='bar')
+
+    plt.title("BAM Value vs. Average Difference")
+    plt.xlabel("BAM1 Score")                    # Or 'BAM2 Score'
+    plt.ylabel("Avg(BAM1 Score - BAM2 Score)")  # Avg(BAM2 Score - BAM1 Score) 
+    plt.xlim([-2, 62])                          # Helpful for plotting MAPQ, which has range [1-60]
+
+    basename = f.split('.')[0]
+    plt.savefig("{}.pdf".format(basename))
