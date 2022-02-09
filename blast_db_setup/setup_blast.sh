5938663a6952cde355f2c988c892b54f95e069a8
David Streid <david.streid@variantyx.com>
2022-02-09 16:07:05 -0500

    Adding validation of db_name

--- blast_db_setup/setup_blast.sh
+++ blast_db_setup/setup_blast.sh
@@ -3,6 +3,51 @@
 available_versions=$(curl ftp://ftp.ncbi.nih.gov/blast/executables/blast+/ 2>/dev/null | rev | cut -d' ' -f1 | rev | grep -oE "[0-9]+.[0-9]+.[0-9]+")
 latest_version=$(curl ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/ 2> /dev/null | grep LATEST | sed 's/.*-> //g')
 
+db_name_options="\t+-----------------------------+------------------------------------------------+\n"
+db_name_options+="\t File Name                    | Content Description                           \n"
+db_name_options+="\t+-----------------------------+------------------------------------------------+\n"
+db_name_options+="\tnr.*tar.gz                    | Non-redundant protein sequences from GenPept,\n"
+db_name_options+="\t                                Swissprot, PIR, PDF, PDB, and NCBI RefSeq\n"
+db_name_options+="\tnt.*tar.gz                    | Partially non-redundant nucleotide sequences from \n"
+db_name_options+="\t                                all traditional divisions of GenBank, EMBL, and DDBJ\n"
+db_name_options+="\t                                excluding GSS,STS, PAT, EST, HTG, and WGS.\n"
+db_name_options+="\tlandmark.tar.gz               | Proteome of 27 model organisms, see \n"
+db_name_options+="\t                                https://blast.ncbi.nlm.nih.gov/smartblast/smartBlast.cgi?CMD=Web&PAGE_TYPE=BlastDocs#searchSets\n"
+db_name_options+="\t16S_ribosomal_RNA             | 16S ribosomal RNA (Bacteria and Archaea type strains)\n"
+db_name_options+="\t18S_fungal_sequences.tar.gz   | 18S ribosomal RNA sequences (SSU) from Fungi type and reference material (BioProject PRJNA39195)\n"
+db_name_options+="\t28S_fungal_sequences.tar.gz   | 28S ribosomal RNA sequences (LSU) from Fungi type and reference material (BioProject PRJNA51803)\n"
+db_name_options+="\tITS_RefSeq_Fungi.tar.gz       | Internal transcribed spacer region (ITS) from Fungi type and reference material (BioProject PRJNA177353)\n"
+db_name_options+="\tITS_eukaryote_sequences.tar.gz| Internal transcribed spacer region (ITS) for eukaryotic sequences\n"
+db_name_options+="\tLSU_eukaryote_rRNA.tar.gz     | Large subunit ribosomal RNA sequences for eukaryotic sequences\n"
+db_name_options+="\tLSU_prokaryote_rRNA.tar.gz    | Large subunit ribosomal RNA sequences for prokaryotic sequences\n"
+db_name_options+="\tSSU_eukaryote_rRNA.tar.gz     | Small subunit ribosomal RNA sequences for eukaryotic sequences\n"
+db_name_options+="\tref_euk_rep_genomes*tar.gz    | Refseq Representative Eukaryotic genomes (1000+ organisms)\n"
+db_name_options+="\tref_prok_rep_genomes*tar.gz   | Refseq Representative Prokaryotic genomes (5700+ organisms)\n"
+db_name_options+="\tref_viruses_rep_genomes*tar.gz   | Refseq Representative Virus genomes (9000+ organisms)\n"
+db_name_options+="\tref_viroids_rep_genomes*tar.gz   | Refseq Representative Viroid genomes (46 organisms)\n"
+db_name_options+="\trefseq_protein.*tar.gz        | NCBI protein reference sequences\n"
+db_name_options+="\trefseq_rna.*tar.gz            | NCBI Transcript reference sequences\n"
+db_name_options+="\tswissprot.tar.gz              | Swiss-Prot sequence database (last major update)\n"
+db_name_options+="\tpataa.*tar.gz                 | Patent protein sequences\n"
+db_name_options+="\tpatnt.*tar.gz                 | Patent nucleotide sequences. Both patent databases\n"
+db_name_options+="\t                                are directly from the USPTO, or from the EPO/JPO\n"
+db_name_options+="\t                                via EMBL/DDBJ\n"
+db_name_options+="\tpdbaa.*tar.gz                 | Sequences for the protein structure from the\n"
+db_name_options+="\t                                Protein Data Bank\n"
+db_name_options+="\tpdbnt.*tar.gz                 | Sequences for the nucleotide structure from the \n"
+db_name_options+="\t                                Protein Data Bank. They are NOT the protein coding\n"
+db_name_options+="\t                                sequences for the corresponding pdbaa entries.\n"
+db_name_options+="\t+-----------------------------+------------------------------------------------+\n"
+db_name_options+="\tFor more info, see https://ftp.ncbi.nlm.nih.gov/blast/documents/blastdb.html"
+
+version_string="\tAvailable blast+ Versions: $(echo ${available_versions})\n\tLatest: ${latest_version}\n"
+help_string="./setup_blast.sh (-v <version>) (-o <os>) (-d <database_name>) (-o <output_path>)\n"
+help_string+="\tversions: $(echo ${available_versions})\n\t\tlatest (default): ${latest_version}\n"
+help_string+="\tos: win64, x64-linux, x64-macosx, x64-win64\n\t\tDefault: x64-linux\n"
+help_string+="\toutput_path: directory to write database files\n"
+help_string+="\tdatabase_names: See https://ftp.ncbi.nlm.nih.gov/blast/documents/blastdb.html\n"
+
+
 while getopts ":v:o:d:p:h" opt; do
     case $opt in
         v) version=${OPTARG}
@@ -13,7 +58,7 @@ while getopts ":v:o:d:p:h" opt; do
         ;;
         p) out_path=${OPTARG}
         ;;
-        h) printf "Available blast+ Versions: $(echo ${available_versions})\n\tLatest: ${latest_version}\n\tTry './setup_blast.sh' to get the latest executables and take it from there\n"; exit 0
+        h) printf "${help_string}\nOr, try './setup_blast.sh' to get the latest executables and take it from there\n"; exit 0
         ;;
     esac 
 done
@@ -38,8 +83,7 @@ has_version=$(echo "${available_versions}" | grep -oE "^${download_version}$")
 
 if [[ -z ${has_version} ]]; then
   echo "[ERROR] Invalid version: ${download_version}"
-  printf "\tAvailable Versions: $(echo ${available_versions})\n"
-  printf "\tLatest: ${latest_version}\n"
+  printf "${version_string}"
   exit 1
 fi 
 untarred_file="ncbi-blast-${download_version}+"
@@ -48,8 +92,16 @@ echo "blast+ Version: ${download_version}"
 echo "TAR file: ${tar_file}"
 echo "version=${version} (latest: ${latest_version})"
 echo "os_input=${os}"
-echo "db_name=${db_name}"
-echo "out_path=${out_path}"
+if [[ ! -z ${db_name} ]]; then
+  if [[ -z $(echo "${db_name_options}" | grep "${db_name}") ]]; then
+    echo "[ERROR] Invalid db_name. See below,"
+    printf "${db_name_options}"
+    echo ""
+    exit 1
+  fi
+  echo "db_name=${db_name}"
+  echo "out_path=${out_path}"
+fi
 echo ""
 
 if [[ -f ${tar_file} ]]; then
@@ -57,9 +109,10 @@ if [[ -f ${tar_file} ]]; then
 elif [[ -f ${untarred_file} ]]; then
   echo "Detected extracted file... skipping download"
 else
-  CMD="curl ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${download_version}/${tar_file} -o ${tar_file} 2> /dev/null"
-  echo ${CMD}
-  eval ${CMD}
+  CMD="curl ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${download_version}/${tar_file} -o ${tar_file}"
+  echo "Downloading..."
+  printf "\t${CMD}\n"
+  eval ${CMD} 2> /dev/null
   if [[ $? -ne 0 ]]; then
     printf "\nDownload failed. Check that version (-v) and os (-o) are valid, or don't specify them to use defaults\n\tReceived version=${download_version} os=${os}\n"
     printf "\tNote - older version don't have all version, e.g. x64-macosx is not present in 2.2.* versions\n"
@@ -75,7 +128,7 @@ if [[ -d ${untarred_file} ]]; then
   echo "Nothing to untar - Using ${untarred_file}"
 elif [[ -f ${tar_file} ]]; then
   echo "Extracting contents..."
-  tar -zxvf ${tar_file}
+  tar -zxvf ${tar_file} 2> /dev/null
   echo "Extraction done."
 else
   echo "No tarred/untarred file. Exiting..."
@@ -115,49 +168,6 @@ if [[ ! -z ${db_name} ]]; then
   echo "Blast executables: $(realpath ncbi-blast-2.12.0+/bin)"
   echo "EXPORT BLASTDB=$(realpath ${out_dir})"
 else
-  echo "+-----------------------------+------------------------------------------------+"
-  echo " File Name                    | Content Description                           "
-  echo "+-----------------------------+------------------------------------------------+"
-  echo "README                        | README for this subdirectory (this file)"
-  echo "nr.*tar.gz                    | Non-redundant protein sequences from GenPept,"
-  echo "                                Swissprot, PIR, PDF, PDB, and NCBI RefSeq"
-  echo "nt.*tar.gz                    | Partially non-redundant nucleotide sequences from "
-  echo "                                all traditional divisions of GenBank, EMBL, and DDBJ"
-  echo "                                excluding GSS,STS, PAT, EST, HTG, and WGS."
-  echo "landmark.tar.gz               | Proteome of 27 model organisms, see "
-  echo "                                https://blast.ncbi.nlm.nih.gov/smartblast/smartBlast.cgi?CMD=Web&PAGE_TYPE=BlastDocs#searchSets"
-  echo "16S_ribosomal_RNA             | 16S ribosomal RNA (Bacteria and Archaea type strains)"
-  echo "18S_fungal_sequences.tar.gz   | 18S ribosomal RNA sequences (SSU) from Fungi type and reference material (BioProject PRJNA39195)"
-  echo "28S_fungal_sequences.tar.gz   | 28S ribosomal RNA sequences (LSU) from Fungi type and reference material (BioProject PRJNA51803)"
-  echo "ITS_RefSeq_Fungi.tar.gz       | Internal transcribed spacer region (ITS) from Fungi type and reference material (BioProject PRJNA177353)"
-  echo "ITS_eukaryote_sequences.tar.gz| Internal transcribed spacer region (ITS) for eukaryotic sequences"
-  echo "LSU_eukaryote_rRNA.tar.gz     | Large subunit ribosomal RNA sequences for eukaryotic sequences"
-  echo "LSU_prokaryote_rRNA.tar.gz    | Large subunit ribosomal RNA sequences for prokaryotic sequences"
-  echo "SSU_eukaryote_rRNA.tar.gz     | Small subunit ribosomal RNA sequences for eukaryotic sequences"
-  echo "ref_euk_rep_genomes*tar.gz    | Refseq Representative Eukaryotic genomes (1000+ organisms)"
-  echo "ref_prok_rep_genomes*tar.gz   | Refseq Representative Prokaryotic genomes (5700+ organisms)"
-  echo "ref_viruses_rep_genomes*tar.gz   | Refseq Representative Virus genomes (9000+ organisms)"
-  echo "ref_viroids_rep_genomes*tar.gz   | Refseq Representative Viroid genomes (46 organisms)"
-  echo "refseq_protein.*tar.gz        | NCBI protein reference sequences"
-  echo "refseq_rna.*tar.gz            | NCBI Transcript reference sequences"
-  echo "swissprot.tar.gz              | Swiss-Prot sequence database (last major update)"
-  echo "pataa.*tar.gz                 | Patent protein sequences"
-  echo "patnt.*tar.gz                 | Patent nucleotide sequences. Both patent databases"
-  echo "                                are directly from the USPTO, or from the EPO/JPO"
-  echo "                                via EMBL/DDBJ"
-  echo "pdbaa.*tar.gz                 | Sequences for the protein structure from the"
-  echo "                                Protein Data Bank"
-  echo "pdbnt.*tar.gz                 | Sequences for the nucleotide structure from the "
-  echo "                                Protein Data Bank. They are NOT the protein coding"
-  echo "                                sequences for the corresponding pdbaa entries."
-  echo "taxdb.tar.gz                  | Additional taxonomy information for the databases "
-  echo "                                listed here  providing common and scientific names"
-  echo "FASTA/                        | Subdirectory for FASTA formatted sequences"
-  echo "v4/                           | BLAST databases in version 4 (v4).  These files are no"
-  echo "                                longer being updated."
-  echo "cloud/	                      | Subdirectory of databases for BLAST AMI; see"
-  echo "                                http://1.usa.gov/TJAnEt"
-  echo "+-----------------------------+------------------------------------------------+"
   echo ""
   echo "For more info, see https://ftp.ncbi.nlm.nih.gov/blast/documents/blastdb.html"
   echo ""
