4eb89cedb149d7910637eec339941a6c1646dfc9
David Streid <david.streid@variantyx.com>
2022-05-02 17:07:59 -0400

    Adding identity_summary.tsv

--- blast_db_setup/contamination/analyze.py
+++ blast_db_setup/contamination/analyze.py
@@ -3,10 +3,10 @@ import collections
 import matplotlib.pyplot as plt
 
 EXPECTED_BLASTNAMES = set()   # BLAST `sblastnames` that are ignored, e.g. if looking for contamination in human saliva, add "primates"
-OUTPUT_TSV = 'output.tsv'
-IDENTITY_TSV = 'query_identities.tsv'
 
-VALID_ID_PROPORTION = 0.5
+OUTPUT_TSV = 'blast_result_analysis.tsv'
+IDENTITY_TSV = 'query_identities.tsv'
+IDENTITY_SUMMARY_TSV = 'identity_summary.tsv'
 
 MAX_E_VALUE = 0.1                     # E-Values greater than this are filtered out, NOTE - "E=1" means 1 result expected by chance
 MAX_MAGNITUDE_DIFFERENCE_ALLOWED = 3  # E-Values more than 10^x the best hit are filtered out, e.g. "3" will filter out anything with E-value > (1000 * lowest E) 
@@ -14,7 +14,11 @@ MAX_MAGNITUDE_DIFFERENCE_ALLOWED = 3  # E-Values more than 10^x the best hit are
 class blast_result:
   @staticmethod
   def get_header():
-    return ['qaccver', 'pident', 'evalue', 'magnitude_of_best_result', 'proportion_valid_blast_results', 'bitscore', 'scomnames', 'sblastnames', 'sskingdoms']
+    return ['qaccver', 'pident', 'evalue', 'magnitude_of_best_result', 'proportion_valid_blast_results', 'total_results', 'bitscore', 'scomnames', 'sblastnames', 'sskingdoms']
+  
+  @staticmethod
+  def get_id_header():
+    return ['qaccver', 'scomnames', 'sblastnames']
 
   def __init__(self, qaccver, pident, evalue, bitscore, scomnames, sblastnames, sskingdoms):
     self.qaccver = qaccver
@@ -26,13 +30,15 @@ class blast_result:
     self.sskingdoms = sskingdoms
     self.magnitude = None             # Order of magnitude higher this result is from the next BETTER result
     self.next_magnitude = 0           # Order of magnitude lower this result is from the next WORSE result
-    self.num_valid_blast_results = 0
+    self.prop_valid_blast_results = 0
+    self.total_valid_blast_results = 0
+    self.total_blast_results = 0
     self.best_evalue_result_sblastnames_proportion = 0
 
   def clone(self):
     br = blast_result(self.qaccver, self.pident, self.evalue, self.bitscore, self.scomnames, self.sblastnames, self.sskingdoms)
     br.next_magnitude = self.next_magnitude
-    br.num_valid_blast_results = self.num_valid_blast_results
+    br.prop_valid_blast_results = self.prop_valid_blast_results
     br.best_evalue_result_sblastnames_proportion = self.best_evalue_result_sblastnames_proportion
 
     return br
@@ -40,8 +46,14 @@ class blast_result:
   def set_best_evalue_result_sblastnames_proportion(self, best_evalue_result_sblastnames_proportion):
     self.best_evalue_result_sblastnames_proportion = best_evalue_result_sblastnames_proportion
 
-  def set_num_valid_blast_results_proportion(self, num_valid_blast_results):
-    self.num_valid_blast_results = num_valid_blast_results
+  def set_total_blast_results(self, total_blast_results):
+    self.total_blast_results = total_blast_results
+
+  def set_total_valid_blast_results(self, total_valid_blast_results):
+    self.total_valid_blast_results = total_valid_blast_results
+
+  def set_prop_valid_blast_results(self, prop_valid_blast_results):
+    self.prop_valid_blast_results = prop_valid_blast_results
 
   def set_next_magnitude(self, blast_result):
     self.next_magnitude = ("%.2f" % blast_result.magnitude)
@@ -53,32 +65,19 @@ class blast_result:
     return '\t'.join([self.qaccver, self.scomnames, self.sblastnames])
 
   def to_string(self):
-    return '\t'.join([self.qaccver, str(self.pident), str(self.evalue), str(self.next_magnitude), str(self.num_valid_blast_results), str(self.bitscore), self.scomnames, self.sblastnames, self.sskingdoms])
+    return '\t'.join([self.qaccver, str(self.pident), str(self.evalue), str(self.next_magnitude), str(self.prop_valid_blast_results), str(self.total_blast_results), str(self.bitscore), self.scomnames, self.sblastnames, self.sskingdoms])
 
   def is_the_best_match(self):
     statistically_significant = self.evalue < 1e-50
     evolutionarily_close = self.pident > 0.9
 
 
-def graph_pie(labels, sizes, fname):
-  # https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_features.html
-  plt.title('BLAST IDENTIFICATIONS')
-
-  fig1, ax1 = plt.subplots()
-  ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-  ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-  plt.savefig("%s.pdf" % fname)
-  plt.close()
-
-
 def output_relevant_results(query_dic):
-  reads_with_valid_blast_results = len(query_dic)
-  with open(OUTPUT_TSV, 'w') as out, open(IDENTITY_TSV, 'w') as identity_out:
+  with open(OUTPUT_TSV, 'w') as out:
     header = '\t'.join(blast_result.get_header())
     out.write(f"{header}\n")
     for highest_hit in query_dic.values():
       out.write(f'{highest_hit.to_string()}\n')
-      identity_out.write(f'{highest_hit.to_identity_string()}\n')
 
   print("[OUT] BLAST RESULT SUMMARY")
   print(f"\tIDS & STATISTICS={OUTPUT_TSV}")
@@ -161,7 +160,10 @@ def aggregate_blast_results(query_dic):
       invalid_blast_ids.add(qaccver)
       continue
 
-    best_evalue_result.set_num_valid_blast_results_proportion(num_valid_blast_results / float(total_blast_results_for_qaccver))
+    best_evalue_result.set_total_blast_results(total_blast_results_for_qaccver)
+    best_evalue_result.set_total_valid_blast_results(num_valid_blast_results)
+    best_evalue_result.set_prop_valid_blast_results(num_valid_blast_results / float(total_blast_results_for_qaccver))
+
     identification = best_evalue_result.sblastnames
 
     best_evalue_result_sblastnames_proportion = get_best_result_sblastnames_proportion(blast_result_list, identification)
@@ -188,9 +190,19 @@ class Graph:
   def graph(self):
     assert len(self.x_values) == len(self.y_values)
 
+    # https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_features.html
+    graph_title = self.title.upper() 
+    fname = self.title.replace(" ", "_") + '.pdf'
+
     print("[OUT] GRAPHING")
-    print(f"\tfile={self.title}.pdf (n={len(self.x_values)})")
-    graph_pie(self.x_values, self.y_values, self.title)
+    print(f"\tfile={fname} (n={len(self.x_values)})")
+
+    plt.title(graph_title)
+    _, ax1 = plt.subplots()
+    ax1.pie(self.y_values, labels=self.x_values, autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig(fname)
+    plt.close()
 
 
 def log_settings():
@@ -215,21 +227,39 @@ def parse_identification_summary(aggregated_query_dic):
 
   highest_count_identification = ''
   highest_count = 0
+  total_ct = 0
+
   for identification, id_count in read_ids.items():
     identifications.append(identification)
     identification_counts.append(id_count)
+
+    total_ct += id_count
 
     if id_count > highest_count:
       highest_count = id_count
       highest_count_identification = identification
 
-    print(f"\t{identification}: {id_count}")  
+  proportion_of_highest_result_ct = "%.2f" % (highest_count / float(len(aggregated_query_dic)))
+  print(f"MOST_IDENTIFIED={highest_count_identification} ({proportion_of_highest_result_ct})")
 
-  proportion_of_highest_result_ct = highest_count / float(len(aggregated_query_dic))
-  print(f"MOST_IDENTIFIED={highest_count_identification}")
-  print(f"IDENTIFICATION_PROPORTION={proportion_of_highest_result_ct}")
+  id_tuples = list(zip(identifications, identification_counts))
+  id_tuples.sort(key=lambda it: it[1], reverse=True)
 
-  return Graph(identifications, identification_counts, 'blast_results')
+  with open(IDENTITY_TSV, 'w') as identity_out, open(IDENTITY_SUMMARY_TSV, 'w') as identity_summary_out:
+    header = '\t'.join(blast_result.get_id_header())
+    identity_out.write(f"{header}\n")
+    for highest_hit in aggregated_query_dic.values():
+      identity_out.write(f'{highest_hit.to_identity_string()}\n')
+
+    identity_summary_out.write("ID\tCount\tProportion\n")
+    for it in id_tuples:
+      ct = it[1]
+      proportion = ct/float(total_ct)
+      line = f"{it[0]}\t{ct}\t{proportion}\n"
+      identity_summary_out.write(line)
+      
+
+  return Graph(identifications, identification_counts, 'blast identifications per read id')
 
 
 if __name__ == '__main__':
