00d555ce79bc08a4f45db69baa91727eb4c9fcad
David Streid <david.streid@variantyx.com>
2022-05-04 10:49:31 -0400

    Adding option to only include reads where both reads have the input clipping

--- bam_analysis/samtools_ext/README.md
+++ bam_analysis/samtools_ext/README.md
@@ -3,22 +3,99 @@ Utils that seem like they could be in samtools
 
 `extract_double_clipped_reads.sh` - Outputs `double_clipped_reads.sam` & `double_clipped_reads.fa` files with only the double-clipped reads from an input SAM
 `isolate_clipped_reads.sh` - Outputs `*_clipped.sam` & `*_clipped_collated.bam` which are SAM/BAM files that have only the filtered double-soft/hard clipped reads.
-* `*_clipped.sam` - Only reads with clippings
-* `*_clipped_collated.bam` - All reads where one of the read pairs has clipping
-
 
 ## isolate_clipped_reads.sh
+### Outputs
+* `*_clipped.sam` - Reads w/ hard and/or soft clipping
+* `*_clipped_collated.bam` - Collated `*_clipped.sam` where RGIDs are together
+* `*_clipped_rgid.txt` - RGIDs matching input clipping filters
+
+### Run
+#### Extract all read pairs where both reads have soft-clipping
 ```
-./isolate_clipped_reads.sh -b sample.bam -f S
-BAM=sample.bam
-FILTER=S
-	filter - H=Hard	S=Soft	HS=Hard & Soft
-Extracting RGIDs of clipped BAM reads: soft_clipped_rgid.txt
-REGEX=[0-9]+[S].+[S]$
-Found 12 clipped RGIDs
-Writing partial_clipped.sam
+../isolate_clipped_reads.sh -f sample.sam -c S -p
+[INPUTS]
+BAM=sample.sam
+CLIPPING_FILTER=S
+PAIRED_CLIPPING_FILTER=TRUE
+
+Extracting RGIDs of clipped BAM reads: S_clipped_rgid.txt
+REGEX="[0-9]+[S].+[S]$"
+Found 4329 clipped RGIDs
+
+Including only RGIDs w/ clipping of both paired-reads
+Found 2003 RGIDs with clipping of both pairs
+
+Writing sample_clipped.sam
 	headers...
 	reads...
-Writing Collated BAM=sample_clipped_collated.bam
+
+Shuffling/Grouping reads into collated BAM=sample_clipped_collated.bam
+
 Done.
 ```
+#### Extract all reads pairs where one of the reads have soft-clipping
+```
+../isolate_clipped_reads.sh -f sample.sam -c H
+[INPUTS]
+BAM=sample.sam
+CLIPPING_FILTER=H
+PAIRED_CLIPPING_FILTER=
+
+Extracting RGIDs of clipped BAM reads: H_clipped_rgid.txt
+REGEX="[0-9]+[H].+[H]$"
+Found 3 clipped RGIDs
+
+Including all RGIDs w/ clipping of at least one paired-read
+
+Writing sample_clipped.sam
+	headers...
+	reads...
+
+Shuffling/Grouping reads into collated BAM=sample_clipped_collated.bam
+
+Done.
+```
+
+#### Extract all reads pairs where both reads have soft OR hard clipping
+```
+[INPUTS]
+BAM=sample.sam
+CLIPPING_FILTER=H
+PAIRED_CLIPPING_FILTER=
+
+Extracting RGIDs of clipped BAM reads: H_clipped_rgid.txt
+REGEX="[0-9]+[H].+[H]$"
+Found 3 clipped RGIDs
+
+Including all RGIDs w/ clipping of at least one paired-read
+
+Writing sample_clipped.sam
+	headers...
+	reads...
+
+Shuffling/Grouping reads into collated BAM=sample_clipped_collated.bam
+
+Done.
+[pplnuser@sga01 test_3]$ ../isolate_clipped_reads.sh -f sample.sam -c HS -p
+[INPUTS]
+BAM=sample.sam
+CLIPPING_FILTER=HS
+PAIRED_CLIPPING_FILTER=TRUE
+
+Extracting RGIDs of clipped BAM reads: HS_clipped_rgid.txt
+REGEX="[0-9]+[HS].+[HS]$"
+Found 4332 clipped RGIDs
+
+Including only RGIDs w/ clipping of both paired-reads
+Found 2003 RGIDs with clipping of both pairs
+
+Writing sample_clipped.sam
+	headers...
+	reads...
+
+Shuffling/Grouping reads into collated BAM=sample_clipped_collated.bam
+
+Done.
+```
+
--- bam_analysis/samtools_ext/isolate_clipped_reads.sh
+++ bam_analysis/samtools_ext/isolate_clipped_reads.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Isolates hard/soft clipped reads in SAM file
 
-options="\tfilter - H=Hard\tS=Soft\tHS=Hard & Soft\n"
-help_string="./isolate_clipped_reads.sh -b {BAM} [-f filter]\n${options}"
+options="\t-f, file: SAM/BAM file\n\t-c, clipping: H=Hard\tS=Soft\tHS=Hard & Soft\n\t-p, paried-clipping: Flag to only include reads where both reads have the \"-c\" clipping"
+help_string="./isolate_clipped_reads.sh -f file [-c clipping] [-p paired-clipping]\n${options}"
 
-while getopts ":b:f:h" opt; do
+while getopts ":f:c:ph" opt; do
     case $opt in
-        b) INPUT_BAM=${OPTARG}
+        f) INPUT_BAM=${OPTARG}
         ;;
-        f) FILTER=${OPTARG}
+        c) CLIPPING_FILTER=${OPTARG}
         ;;
-        h) printf "${help_string}"
+        p) PAIRED_CLIPPING_FILTER="TRUE"
+        ;; 
+        h) printf "${help_string}" && exit 0
         ;;
     esac 
 done
@@ -21,37 +23,60 @@ if [[ ! -f ${INPUT_BAM} ]]; then
   exit 1
 fi
 
-if [[ -z ${FILTER} ]]; then
-  FILTER=HS # Default to both Hard & Soft-Clipped Reads
+if [[ -z ${CLIPPING_FILTER} ]]; then
+  CLIPPING_FILTER=HS # Default to both Hard & Soft-Clipped Reads
 fi
 
-REGEX="[0-9]+[${FILTER}].+[${FILTER}]$"
-
+echo "[INPUTS]"
 echo "BAM=${INPUT_BAM}"
-echo "FILTER=${FILTER}"
-printf "${options}"
+echo "CLIPPING_FILTER=${CLIPPING_FILTER}"
+echo "PAIRED_CLIPPING_FILTER=${PAIRED_CLIPPING_FILTER}"
+echo ""
 
+# REF: https://www.biostars.org/p/137461/#137631
+CLIPPED_RGID_FILE="${CLIPPING_FILTER}_clipped_rgid.txt"
+echo "Extracting RGIDs of clipped BAM reads: ${CLIPPED_RGID_FILE}"
+REGEX="[0-9]+[${CLIPPING_FILTER}].+[${CLIPPING_FILTER}]$"
+echo "REGEX=\"${REGEX}\""
+samtools view ${INPUT_BAM} | awk -v REGEX="${REGEX}" '$6~/'"$REGEX"'/{print $1}' | sort -k1,1 > ${CLIPPED_RGID_FILE}
+echo "Found $(wc -l ${CLIPPED_RGID_FILE} | grep -oE "[0-9]+") clipped RGIDs"
+echo ""
 
-OUTPUT_BAM="$(echo ${INPUT_BAM} | cut -d'.' -f1)_clipped.sam"
+# If filtering on both paired reads, check each RGID for two entries in our file of RGIDs w/ clipped-reads
+tmp_file=".${CLIPPED_RGID_FILE}"
+touch ${tmp_file}
+if [[ ! -z ${PAIRED_CLIPPING_FILTER} ]]; then
+  echo "Including only RGIDs w/ clipping of both paired-reads"
+  for rgid in $(cat ${CLIPPED_RGID_FILE} | uniq); do
+    if [[ 1 -ne $(grep "${rgid}" ${CLIPPED_RGID_FILE} | wc -l) ]]; then
+      echo ${rgid} >> ${tmp_file}
+    fi
+  done
+  num_filtered=$(wc -l ${tmp_file} | grep -oE "[0-9]+")
+  echo "Found ${num_filtered} RGIDs with clipping of both pairs"
+  if [[ 0 -eq ${num_filtered} ]]; then
+    echo "No reads match the provided filters. Not writing BAM & exiting."
+    exit 0
+  fi 
+else
+  echo "Including all RGIDs w/ clipping of at least one paired-read"
+  sort ${CLIPPED_RGID_FILE} | uniq > ${tmp_file}
+fi
+mv ${tmp_file} ${CLIPPED_RGID_FILE}
+echo ""
 
-SC_RGID_FILE=soft_clipped_rgid.txt
-
-# https://www.biostars.org/p/137461/#137631
-echo "Extracting RGIDs of clipped BAM reads: ${SC_RGID_FILE}"
-echo "REGEX=${REGEX}"
-samtools view ${INPUT_BAM} | awk -v REGEX="${REGEX}" '$6~/'"$REGEX"'/{print $1}' | sort -k1,1 | uniq > ${SC_RGID_FILE}
-
-echo "Found $(wc -l ${SC_RGID_FILE} | grep -oE "[0-9]+") clipped RGIDs"
-
-echo "Writing ${OUTPUT_BAM}"
+CLIPPED_READ_BAM="$(basename ${INPUT_BAM} | cut -d'.' -f1)_clipped.sam"
+echo "Writing ${CLIPPED_READ_BAM}"
 printf "\theaders...\n"
-samtools view -H ${INPUT_BAM} > ${OUTPUT_BAM}
-
+samtools view -H ${INPUT_BAM} > ${CLIPPED_READ_BAM}
 printf "\treads...\n"
-samtools view ${INPUT_BAM} | fgrep -w -f ${SC_RGID_FILE} >> ${OUTPUT_BAM}
+samtools view ${INPUT_BAM} | fgrep -w -f ${CLIPPED_RGID_FILE} >> ${CLIPPED_READ_BAM}
+echo ""
 
-COLLATED_BAM="$(echo ${OUTPUT_BAM} | cut -d'.' -f1)___collated.bam"
-echo "Writing Collated BAM=${COLLATED_BAM}"
-samtools collate -o ${COLLATED_BAM} ${OUTPUT_BAM}
+COLLATED_BAM="$(echo ${CLIPPED_READ_BAM} | cut -d'.' -f1)_collated.bam"
+echo "Shuffling/Grouping reads into collated BAM=${COLLATED_BAM}"
+samtools collate -o ${COLLATED_BAM} ${CLIPPED_READ_BAM}
+echo ""
 
 echo "Done."
+
