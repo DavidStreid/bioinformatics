da7c647a70561c08c0dac61a538ec8f7c091102b
DavidStreid <david.streid.nyc@gmail.com>
2023-09-29 11:13:18 -0400

    Create helpful BLAST commands

---
+++ blast_db_setup/helpful_cmds.sh
@@ -0,0 +1,6 @@
+## Retrieve Sequences
+```
+$ blastdbcmd -db ref_viruses_rep_genomes -taxids 11137
+>NC_002645.1 Human coronavirus 229E, complete genome
+ACTTAAGTACCTTATCTATCTACAGATAGAAAAGTTGCTTTTTAGACTTTGT...
+```
