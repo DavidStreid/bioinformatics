8bbef7551fbd1f07bf98aa42b1f26789511dd6e1
David Streid <david.streid@variantyx.com>
2024-11-19 10:27:49 -0500

    Writing file with changes

--- vcf_tools/compare/compare_vcf_by_position.sh
+++ vcf_tools/compare/compare_vcf_by_position.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Compares two VCF files, treating their position as IDs
+# NOTE - writes the output to a file
+
+# TODO
+# PATH_TO_BEDTOOLS=""
+# PATH="${PATH}:${PATH_TO_BEDTOOLS}"
 
 vcf1=$1
 vcf2=$2
@@ -12,10 +17,25 @@ if [[ ! -f ${vcf1} || ! -f ${vcf2} ]]; then
   exit 1
 fi
 
-same_in_1=$(bedtools intersect -u -a  ${vcf1} -b ${vcf2} | wc -l)
-same_in_2=$(bedtools intersect -u -a  ${vcf1} -b ${vcf2} | wc -l)
-new_diffs=$(bedtools subtract -a ${vcf1} -b ${vcf2} | wc -l)
-old_diffs=$(bedtools subtract -a ${vcf2} -b ${vcf1} | wc -l)
+
+f_same_1="shared_vcf1.tsv"
+f_same_2="shared_vcf2.tsv"
+f_diff_1="vcf1_only.tsv"
+f_diff_2="vcf2_only.tsv"
+cmds=("bedtools intersect -u -a ${vcf1} -b ${vcf2} > ${f_same_1}"
+"bedtools intersect -u -a ${vcf1} -b ${vcf2} > ${f_same_2}"
+"bedtools subtract -a ${vcf1} -b ${vcf2} > ${f_diff_1}"
+"bedtools subtract -a ${vcf2} -b ${vcf1} > ${f_diff_2}")
+for cmd in "${cmds[@]}"; do
+  echo "${cmd}"
+  eval "${cmd}"
+done
+
+
+same_in_1=$(cat ${f_same_1} | wc -l)
+same_in_2=$(cat ${f_same_2} | wc -l)
+new_diffs=$(cat ${f_diff_1} | wc -l)
+old_diffs=$(cat ${f_diff_2} | wc -l)
  
 
 echo "vcf1_same=${same_in_1}"
