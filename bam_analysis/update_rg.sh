59efb1c858126239d41b55551eef96857718196a
DavidStreid <david.streid.nyc@gmail.com>
2025-02-09 08:25:23 -0500

    SFX & PL

--- bam_analysis/update_rg.sh
+++ bam_analysis/update_rg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Updates the readgroup of an input SAM to the input ID
 
 ID=$1
 SAM=$2
-SFX="bam"
+SFX=$3 # "bam" or "cram"
+PL="Illumina"
 if [[ -z ${ID} ]]; then
   echo "Need ID"
   exit 1
@@ -11,8 +13,12 @@ if [[ ! -f ${SAM} ]]; then
   echo "Need SAM"
   exit 1
 fi
+if [[ -z ${SFX} ]]; then
+  SFX=$(basename ${SAM} | rev | cut -f1 -d'.' | rev)
+  printf "UPDATING SFX\t${SFX}\n"
+fi
 
 FMT=$(echo ${SFX} | awk '{print toupper($0)}')
 OUT="$(basename ${SAM} | rev | cut -f2- -d'.' | rev).${SFX}"
-printf "FMT=${FMT}\tOUT=${OUT}\n"
-samtools addreplacerg -w -r "ID:${ID}\tLB:Seq\tPL:Illumina\tSM:${ID}" ${SAM} -O ${FMT} -o ${OUT}
+printf "FMT=${FMT}\tOUT=${OUT}\tPL=${PL}\n"
+samtools addreplacerg -@ 16 -w -r "ID:${ID}\tLB:Seq\tPL:${PL}\tSM:${ID}" ${SAM} -O ${FMT} -o ${OUT}
